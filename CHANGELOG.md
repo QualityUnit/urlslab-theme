5329bbebe494100491199413528991345d17e691
Martin Stepanek <martin@stepanek.sk>
2022-05-30 13:46:26 +0200

    feat: bump version to 1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+### 1.0.1: May 30th, 2022
+
+- Bug Fixes and Improvements
+
+
 ### 1.0.0: April 12th, 2022
 
 - The initial release of the new structure
--- lib/assets.php
+++ lib/assets.php
@@ -9,7 +9,7 @@ add_action(
 	function () {
 		wp_enqueue_style( 'font-poppins', 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap', false, '1' );
 		wp_enqueue_style( 'font-opensans', 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap', false, '1' );
-		wp_enqueue_style( 'app', get_template_directory_uri() . '/assets/dist/app' . wpenv() . '.css', false, '1.0.0' );
+		wp_enqueue_style( 'app', get_template_directory_uri() . '/assets/dist/app' . wpenv() . '.css', false, '1.0.1' );
 
 		wp_deregister_script( 'wp-embed' );
 	},
@@ -25,6 +25,6 @@ add_action(
 	'wp_footer',
 	function () {
 		wp_enqueue_script( 'jquery' );
-		wp_enqueue_script( 'app_js', get_template_directory_uri() . '/assets/dist/app' . wpenv() . '.js', false, '1.0.0', true );
+		wp_enqueue_script( 'app_js', get_template_directory_uri() . '/assets/dist/app' . wpenv() . '.js', false, '1.0.1', true );
 	}
 );
