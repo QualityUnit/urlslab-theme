1928e2ecd37e16bd8e1e05729b767e60de0d277b
Jozef Remen <jozef.remen@gmail.com>
2022-06-27 11:30:40 +0200

    [561] feat: adding extra faq shortcode (#89)
    * feat: adding extra faq shortcode
    
    * fix: added faq reference to shortcodes

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -1,3 +1,4 @@
 <?php
 	require_once get_template_directory() . '/lib/shortcodes/language-switcher.php';
 	require_once get_template_directory() . '/lib/shortcodes/signup-form.php';
+	require_once get_template_directory() . '/lib/shortcodes/extra-faq.php';
