849548b7e95e9137aef857486a65c3d68de7ba6f
Jozef Remeň <jozef.remen@gmail.com>
2022-12-13 08:16:20 +0100

    fix: escaping variable

--- templates/header.php
+++ templates/header.php
@@ -2,7 +2,7 @@
 	<div class="wrapper">
 		<div class="Header__logo">
 			<a href="<?= esc_url( home_url( '/' ) ); ?>">
-				<img src="<?= esc_url( get_template_directory_uri() ); ?>/assets/images/urlslab-logo.svg?<?= THEME_VERSION; ?>" alt="<?php bloginfo( 'name' ); ?>">
+				<img src="<?= esc_url( get_template_directory_uri() . '/assets/images/urlslab-logo.svg?' . THEME_VERSION ); ?>" alt="<?php bloginfo( 'name' ); ?>">
 			</a>
 		</div>
 		<nav class="Header__navigation">
