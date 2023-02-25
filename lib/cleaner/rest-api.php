31b682246e82bb038c6ced046eb56090947965a2
Martin Stepanek <martin@stepanek.sk>
2023-02-25 18:23:57 +0100

    chore: clean and update files

--- lib/cleaner/rest-api.php
+++ lib/cleaner/rest-api.php
@@ -2,18 +2,6 @@
 
 
 /**
-  * Disable REST API
-  */
-
-if ( ! is_user_logged_in() ) {
-	add_filter( 'rest_authentication_errors', 'wp_snippet_disable_rest_api' );
-	function wp_snippet_disable_rest_api( $access ) {
-		return new WP_Error( 'rest_disabled', __( 'The WordPress REST API has been disabled.' ), array( 'status' => rest_authorization_required_code() ) );
-	}
-}
-
-
-/**
   * Fix showing of users in REST API
   */
 
