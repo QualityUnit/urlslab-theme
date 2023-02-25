d8a2717b963e5f96298d8a6466ba07ce116e8679
Martin Stepanek <martin@stepanek.sk>
2023-02-25 18:36:01 +0100

    fix: remove language-switcher

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -1,4 +1,3 @@
 <?php
-	require_once get_template_directory() . '/lib/shortcodes/language-switcher.php';
 	require_once get_template_directory() . '/lib/shortcodes/signup-form.php';
 	require_once get_template_directory() . '/lib/shortcodes/extra-faq.php';
