cf747bcd8a85f642b9c4eb2619666772d7b7fad0
Jozef Remeň <jozef.remen@gmail.com>
2023-07-07 11:46:23 +0200

    feat: videos post type and categories

--- lib/post-types.php
+++ lib/post-types.php
@@ -4,3 +4,4 @@
 	require_once get_template_directory() . '/lib/post-types/glossary.php';
 	require_once get_template_directory() . '/lib/post-types/guide.php';
 	require_once get_template_directory() . '/lib/post-types/pricing-tables.php';
+	require_once get_template_directory() . '/lib/post-types/videos.php';
