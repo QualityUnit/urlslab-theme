f2d5b95cfa24053c19127f9d48b7c8d0c354045f
Martin Stepanek <martin@stepanek.sk>
2023-07-19 09:28:13 +0200

    chore: new release pipeline

---
+++ .github/scripts/build-zip.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Inspired by Elementor Releasing Pipeline https://github.com/elementor/elementor
+set -eo pipefail
+
+if [[ -z "$PACKAGE_VERSION" ]]; then
+	echo "Missing PACKAGE_VERSION env var"
+	exit 1
+fi
+
+rm -rf node_modules vendor .git .idea .github .husky *.md *.lock .editorconfig .eslintignore .eslintrc .gitignore .stylelintrc composer.json gulpfile.js commitlint.config.js package.json phpcs.xml
+
+PLUGIN_ZIP_FILENAME="urlslab_${PACKAGE_VERSION}.zip"
+dir_name="urlslab"
+mkdir "$dir_name"
+for file in *; do
+    if [ "$file" != "$dir_name" ]; then
+        mv "$file" "$dir_name"
+    fi
+done
+
+zip -r $PLUGIN_ZIP_FILENAME ./urlslab/ -x "*.zip"
+echo "PLUGIN_ZIP_FILENAME=${PLUGIN_ZIP_FILENAME}" >> $GITHUB_ENV
+echo "PLUGIN_ZIP_PATH=./**/*" >> $GITHUB_ENV
