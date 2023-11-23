5079327dee81e81f179f88a3e85dacb70ba10306
thujcak <thujcak@qualityunit.com>
2023-11-23 09:56:27 +0100

    feat: removed code for language switcher

--- assets/scripts/app/app.js
+++ assets/scripts/app/app.js
@@ -81,58 +81,6 @@
 		mobileMediaQuery.addEventListener( 'change', handleWindowSizeChange );
 	} );
 
-	// Commented because we DON'T NEED it NOW
-	/* Language switcher */
-	// function hideLanguageSwitcher( target ) {
-	// 	if ( target.classList.contains( 'visible' ) ) {
-	// 		target.classList.remove( 'visible' );
-	// 		setTimeout( () => {
-	// 			target.classList.remove( 'active' );
-	// 		}, 200 );
-	// 	}
-	// }
-
-	// if ( query( '.Header__flags #languageSwitcher-toggle' ) !== null ) {
-	// 	const langSwitcherToggle = query(
-	// 		'.Header__flags #languageSwitcher-toggle'
-	// 	);
-
-	/* Gets region of active language and activates mobile switcher for such region */
-	// const langActive = langSwitcherToggle.parentElement.getAttribute(
-	// 	'lang'
-	// );
-	// const matchRegion = langSwitcherToggle.parentElement.querySelector(
-	// 	`.Header__flags--item[lang=${ langActive }]`
-	// ).dataset.region;
-
-	// document.querySelector(
-	// 	`#${ matchRegion }.input--region`
-	// ).checked = true;
-
-	/* Toggles language switcher */
-	// 	const langSwitcherMenu = query( '.Header__flags--mainmenu' );
-	// 	langSwitcherToggle.addEventListener( 'click', ( event ) => {
-	// 		event.stopPropagation();
-	// 		if ( ! langSwitcherMenu.classList.contains( 'visible' ) ) {
-	// 			langSwitcherMenu.dataset.active = 'active';
-	// 			langSwitcherMenu.classList.add( 'active' );
-	// 			setTimeout( () => {
-	// 				langSwitcherMenu.classList.add( 'visible' );
-	// 			}, 200 );
-	// 		}
-	// 		hideLanguageSwitcher( langSwitcherMenu );
-	// 	} );
-	//
-	// 	document.addEventListener( 'click', ( event ) => {
-	// 		if (
-	// 			! event.target.classList.contains( 'Header__flags--mainmenu' )
-	// 		) {
-	// 			event.stopPropagation();
-	// 			hideLanguageSwitcher( langSwitcherMenu );
-	// 		}
-	// 	} );
-	// }
-
 	/* Data Href */
 	if ( queryAll( '[data-href]' ).length > 0 ) {
 		queryAll( '[data-href]' ).forEach( ( element ) => {
