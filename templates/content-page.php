a60fb9e9e7180d30b862c1a9c70216bf5953bb97
thujcak <thujcak@qualityunit.com>
2023-11-28 11:58:05 +0100

    feat: new sections at homepage and redesign mobile styles

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,13 +1,13 @@
 <?php
 the_content();
 
-if ( ! is_page( array( 'sitemap' ) ) ) {
+if ( ! is_page( array( 'sitemap',  ) ) &&  ( ! is_front_page()) )  {
 	?>
 	<div class="SimilarSources SimilarSources--blog">
 		<div class="wrapper">
 			<div class="SimilarSources__title h4"><?php _e( 'Related Articles', 'ms' ); ?></div>
 
-	<?php echo do_shortcode( '[urlslab-related-resources related-count="4" show-image="true" show-summary="true"]' ); ?>
+				<?php echo do_shortcode( '[urlslab-related-resources related-count="4" show-image="true" show-summary="true"]' ); ?>
 		</div>
-	</div>
-	<?php } ?>
+</div>
+<?php } ?>
