e25255b24352606a38306d7ba3cc13cbaefc4d8b
thujcak <thujcak@qualityunit.com>
2023-11-28 12:25:47 +0100

    fix: fixed phpcs errors

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,13 +1,13 @@
 <?php
 the_content();
 
-if ( ! is_page( array( 'sitemap',  ) ) &&  ( ! is_front_page()) )  {
+if ( function_exists( 'is_page' ) && ! is_page( array( 'sitemap' ) ) && function_exists( 'is_front_page' ) && ! is_front_page() ) {
 	?>
 	<div class="SimilarSources SimilarSources--blog">
 		<div class="wrapper">
 			<div class="SimilarSources__title h4"><?php _e( 'Related Articles', 'ms' ); ?></div>
 
-				<?php echo do_shortcode( '[urlslab-related-resources related-count="4" show-image="true" show-summary="true"]' ); ?>
+			<?php echo do_shortcode( '[urlslab-related-resources related-count="4" show-image="true" show-summary="true"]' ); ?>
 		</div>
-</div>
+	</div>
 <?php } ?>
