ef7b3ca47658fefb80020f0cab91b0884077290c
Tomáš Hujčák <134269593+thujcak@users.noreply.github.com>
2024-01-02 10:23:31 +0100

    fix: added redirects for categories (#335)

--- lib/functions.php
+++ lib/functions.php
@@ -6,3 +6,4 @@
 	require_once get_template_directory() . '/lib/functions/import-functions.php';
 	require_once get_template_directory() . '/lib/functions/sidebar-toc.php';
 	require_once get_template_directory() . '/lib/functions/widgets.php';
+	require_once get_template_directory() . '/lib/functions/redirects.php';
---
+++ lib/functions/redirects.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+  * Redirect Features Categories
+  */
+function features_category_redirect() {
+	if ( is_tax( 'features_categories' ) ) {
+		wp_safe_redirect( '/features/', 301 );
+		exit;
+	}
+}
+add_action( 'template_redirect', 'features_category_redirect' );
+
+/**
+  * Redirect About us Categories
+  */
+function about_category_redirect() {
+	if ( is_tax( 'about_categories' ) ) {
+		wp_safe_redirect( '/about/contact', 301 );
+		exit;
+	}
+}
+add_action( 'template_redirect', 'about_category_redirect' );
+
+/**
+ * Redirect Videos Categories
+ */
+function videos_category_redirect() {
+	if ( is_tax( 'videos_categories' ) ) {
+		wp_safe_redirect( '/videos/', 301 );
+		exit;
+	}
+}
+add_action( 'template_redirect', 'videos_category_redirect' );
+
