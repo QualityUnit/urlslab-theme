c48e68252a98504cabb7c17f95139688aeba76ea
Jozef Remeň <jozef.remen@gmail.com>
2024-04-04 09:52:13 +0200

    feat: cookies bar, chatbot loading on cookie

--- base.php
+++ base.php
@@ -17,16 +17,14 @@
 				do_action( 'get_footer' );
 				get_template_part( 'templates/footer' );
 			?>
+
+			<div id="papPlaceholder"></div>
 		</div>
 
-		<?php wp_footer(); ?>
+		<?php 
+		wp_footer();
+		require_once 'base-scripts.php';
+		?>
 
-		<!-- Google Tag Manager -->
-		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-				j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-				'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-			})(window,document,'script','dataLayer','GTM-T2XN5C2');</script>
-		<!-- End Google Tag Manager -->
 	</body>
 </html>
